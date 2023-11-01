c8de9d2766d67379d0d266415113792566617e0c
Jose Dominguez <141463676+jdomi020@users.noreply.github.com>
2023-11-01 22:43:37 +0000

    Jose's First Footer Push

---
+++ src/component/Footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <footer className="footer bg-dark">
+            <div className="copyright" style={{color:"white"}}>
+                Copyright Information 2023
+            </div>
+        </footer>
+    );
+};
